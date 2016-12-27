1e6323fd5f33e393b431225f4a6d4cfa877ae0d0
Chad <chad@chadmott.com>
2016-12-27 08:25:39 -0600

    updating script

--- scratch.sh
+++ scratch.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt-get update -y && sudo apt-get upgrade -y 
 sudo apt-get install apt-transport-https ca-certificates git nano -y
 sudo apt-key adv \
@@ -5,7 +6,7 @@ sudo apt-key adv \
                --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt-get update -y && sudo apt-get install docker-engine docker-compose docker-machine -y
+sudo apt-get update -y && sudo apt-get install docker-engine docker-compose -y
 sudo gpasswd -a ${USER} docker
 sudo su
 curl -L https://github.com/docker/machine/releases/download/v0.8.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && \
